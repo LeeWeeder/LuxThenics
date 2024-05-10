8a7dd1090e36788d86450d38f3db17aa7d4dd180
LeeWeeder <ljmaq239@gmail.com>
2024-05-10 21:12:20 +0800

    Move out ExerciseDao to its own file

--- app/src/main/java/com/leeweeder/luxthenics/domain/model/Exercise.kt
+++ app/src/main/java/com/leeweeder/luxthenics/domain/model/Exercise.kt
@@ -1,12 +1,8 @@
 package com.leeweeder.luxthenics.domain.model
 
-import androidx.room.Dao
 import androidx.room.Entity
 import androidx.room.Index
-import androidx.room.Insert
 import androidx.room.PrimaryKey
-import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 
 @Entity(indices = [Index(value = ["name"], unique = true)])
 data class Exercise(
@@ -15,13 +11,4 @@ data class Exercise(
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
-}
-
-@Dao
-interface ExerciseDao {
-    @Query("SELECT * FROM exercise ORDER BY id DESC LIMIT 10")
-    fun getExercises(): Flow<List<Exercise>>
-
-    @Insert
-    suspend fun insertExercise(item: Exercise)
 }
