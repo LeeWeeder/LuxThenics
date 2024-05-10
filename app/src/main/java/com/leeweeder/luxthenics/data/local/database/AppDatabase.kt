9518e0f6d9c87f29f105186eb6ca5c4bbb7ddc4a
LeeWeeder <ljmaq239@gmail.com>
2024-05-10 22:02:40 +0800

    Add entities to AppDatabase

--- app/src/main/java/com/leeweeder/luxthenics/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/leeweeder/luxthenics/data/local/database/AppDatabase.kt
@@ -2,10 +2,39 @@ package com.leeweeder.luxthenics.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.leeweeder.luxthenics.data.domain.model.Exercise
-import com.leeweeder.luxthenics.data.domain.model.ExerciseDao
+import androidx.room.TypeConverters
+import com.leeweeder.luxthenics.domain.model.Exercise
+import com.leeweeder.luxthenics.domain.model.ExerciseGroup
+import com.leeweeder.luxthenics.domain.model.ExerciseLog
+import com.leeweeder.luxthenics.domain.model.Progression
+import com.leeweeder.luxthenics.domain.model.Routine
+import com.leeweeder.luxthenics.domain.model.RoutineExercise
+import com.leeweeder.luxthenics.domain.model.ExerciseTargetCrossRef
+import com.leeweeder.luxthenics.domain.model.Target
 
-@Database(entities = [Exercise::class], version = 1)
+@Database(
+    entities = [
+        Exercise::class,
+        ExerciseGroup::class,
+        ExerciseLog::class,
+        Progression::class,
+        Routine::class,
+        RoutineExercise::class,
+        Target::class,
+        ExerciseTargetCrossRef::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDao(): ExerciseDao
+    abstract fun exerciseGroupDao(): ExerciseGroupDao
+    abstract fun exerciseLogDao(): ExerciseLogDao
+    abstract fun progressionDao(): ProgressionDao
+    abstract fun routineDao(): RoutineDao
+    abstract fun routineExerciseDao(): RoutineExerciseDao
+    abstract fun targetDao(): TargetDao
+
+    companion object {
+        const val DATABASE_NAME = "luxthenics_db"
+    }
 }
